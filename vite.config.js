2df62c31d27260310bc1787de450a9731361a3ef
Kenny Haggard <khaggard.kh@gmail.com>
2025-03-29 21:44:40 -0500

    Configure deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// vite.config.js
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: '/bible_quiz/' // Ensure this matches your repo name
+});
